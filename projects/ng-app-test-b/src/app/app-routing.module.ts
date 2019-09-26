fe90c220e5168f46ebae046abc00999f4e1faf4d
encisochristian <encisochristianmalana@gmail.com>
2019-09-26 19:14:55 +0800

    Added home page and about page

--- projects/ng-app-test-b/src/app/app-routing.module.ts
+++ projects/ng-app-test-b/src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
